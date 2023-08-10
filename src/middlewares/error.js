5f7ea41fb1ea0ebe7117e7dc96c515383c24c734
Bautista Tosi <tosibautista@gmail.com>
2023-08-09 21:44:36 -0300

    los errores customizados responden con el codigo que creo correcto

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -2,11 +2,26 @@ import { EErros } from '../services/errors/enums.js'
 
 export function errorHandler (error, req, res, next) {
   console.log(error.cause)
-
+  /* hacer los casos */
   switch (error.code) {
     case EErros.INVALID_TYPES_ERROR:
       res
-        .status(400)
+        .status(422)
+        .send({ status: 'error', error: error.name, cause: error.cause })
+      break
+    case EErros.DATABASES_ERROR:
+      res
+        .status(500)
+        .send({ status: 'error', error: error.name, cause: error.cause })
+      break
+    case EErros.INCORRECT_CREDENTIALS_ERROR:
+      res
+        .status(417)
+        .send({ status: 'error', error: error.name, cause: error.cause })
+      break
+    case EErros.ROUTING_ERROR:
+      res
+        .status(404)
         .send({ status: 'error', error: error.name, cause: error.cause })
       break
     default:
